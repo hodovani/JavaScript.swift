81211bb6f9deee4bb9e339bf7758692de66484c7
matvii <matvii@hodovani.uk>
2019-01-19 18:18:06 +0200

    Remove global context and add more tests (#1)
    * Remove context singleton
    
    * Add `import` without throw
    
    * Add `setObject` to JavaScriptSwift
    
    * Add attempt to pass swift closure to JS
    
    * Add testPassSwiftClosureToJavaScript

--- Sources/JavaScriptSwift/JavaScriptSwift.swift
+++ Sources/JavaScriptSwift/JavaScriptSwift.swift
@@ -2,16 +2,21 @@ import JavaScriptCore
 
 @dynamicMemberLookup
 public final class JavaScriptSwift {
+    fileprivate var context = JSContext()!
+
     public enum Error: Swift.Error {
         case notAFunction
+        case exception(String)
     }
 
-    public static let context = JavaScriptSwift()
-    fileprivate let context = JSContext()!
+    /// Initializes `self` with separete context.
+    public init() {
+        context = JSContext()!
+    }
 
     var exception: JavaScriptException? {
-        if let exception = context.exception {
-            let object = JSValue(exception)
+        if let exception = self.context.exception {
+            let object = Value(exception)
             return JavaScriptException(
                 message: object.message.string!,
                 line: object.line.int,
@@ -23,21 +28,40 @@ public final class JavaScriptSwift {
     }
 
     @discardableResult
-    public static func `import`(_ script: String) throws -> JSValue {
-        let value = JSValue(context.context.evaluateScript(script))
-        if let exception = context.exception { throw exception }
+    public func importSafe(_ script: String) throws -> Value {
+        let value = Value(context.evaluateScript(script))
+
+        if let exception = context.exception {
+            throw Error.exception(exception.toString())
+        }
+
         return value
     }
 
     @discardableResult
-    public static func `import`(_ url: URL) throws -> JSValue {
+    public func `import`(_ script: String) -> Value {
+        let value = Value(context.evaluateScript(script))
+
+        if let exception = context.exception {
+            fatalError(exception.toString())
+        }
+
+        return value
+    }
+
+    @discardableResult
+    public func importSafe(_ url: URL) throws -> Value {
         return try `import`(String(contentsOf: url))
+    }
+
+    public func setObject(_ object: Any!, forKeyedSubscript key: (NSCopying & NSObjectProtocol)!) {
+        context.setObject(object, forKeyedSubscript: key)
     }
 }
 
 @dynamicCallable
 @dynamicMemberLookup
-public final class JSValue {
+public struct Value {
     internal let value: JavaScriptCore.JSValue
 
     fileprivate init(_ value: JavaScriptCore.JSValue) {
@@ -61,7 +85,7 @@ struct JavaScriptException: Error, CustomStringConvertible, CustomDebugStringCon
     }
 }
 
-public extension JSValue {
+public extension Value {
     var isUndefined: Bool {
         return value.isUndefined
     }
@@ -99,52 +123,58 @@ public extension JSValue {
     }
 }
 
-extension JSValue: ExpressibleByNilLiteral {
-    public convenience init(nilLiteral _: ()) {
-        self.init(.init(nullIn: JavaScriptSwift.context.context))
+extension Value: ExpressibleByNilLiteral {
+    public init(nilLiteral _: ()) {
+        self.init(.init(nullIn: JSContext()))
     }
 }
 
-extension JSValue: ExpressibleByBooleanLiteral {
-    public convenience init(booleanLiteral value: Bool) {
-        self.init(.init(bool: value, in: JavaScriptSwift.context.context))
+extension Value: ExpressibleByBooleanLiteral {
+    public init(booleanLiteral value: Bool) {
+        self.init(.init(bool: value, in: JSContext()))
     }
 }
 
-extension JSValue: ExpressibleByIntegerLiteral {
-    public convenience init(integerLiteral value: Int32) {
-        self.init(.init(int32: value, in: JavaScriptSwift.context.context))
+extension Value: ExpressibleByIntegerLiteral {
+    public init(integerLiteral value: Int32) {
+        self.init(.init(int32: value, in: JSContext()))
     }
 }
 
-extension JSValue: ExpressibleByFloatLiteral {
-    public convenience init(floatLiteral value: Double) {
-        self.init(.init(double: value, in: JavaScriptSwift.context.context))
+extension Value: ExpressibleByFloatLiteral {
+    public init(floatLiteral value: Double) {
+        self.init(.init(double: value, in: JSContext()))
     }
 }
 
-extension JSValue: ExpressibleByStringLiteral {
-    public convenience init(stringLiteral value: String) {
-        self.init(.init(object: value, in: JavaScriptSwift.context.context))
+extension Value: ExpressibleByStringLiteral {
+    public init(stringLiteral value: String) {
+        self.init(.init(object: value, in: JSContext()))
     }
 }
 
-extension JSValue: Equatable {
-    public static func == (lhs: JSValue, rhs: JSValue) -> Bool {
+extension Value: Equatable {
+    public static func == (lhs: Value, rhs: Value) -> Bool {
         return lhs.value.isEqualWithTypeCoercion(to: rhs.value)
     }
 }
 
-extension JSValue: CustomStringConvertible {
+extension Value: CustomStringConvertible {
     public var description: String {
         return value.toString()
     }
 }
 
+extension Value {
+    public init(object: Any) {
+        self.init(.init(object: object, in: JSContext()))
+    }
+}
+
 public extension JavaScriptSwift {
-    subscript(dynamicMember member: String) -> JSValue {
+    subscript(dynamicMember member: String) -> Value {
         get {
-            return JSValue(context.objectForKeyedSubscript(member))
+            return Value(context.objectForKeyedSubscript(member))
         }
         set {
             context.setObject(newValue.value, forKeyedSubscript: member as NSString)
@@ -152,19 +182,19 @@ public extension JavaScriptSwift {
     }
 }
 
-public extension JSValue {
-    subscript(index: Int) -> JSValue {
+public extension Value {
+    subscript(index: Int) -> Value {
         get {
-            return JSValue(value.atIndex(index))
+            return Value(value.atIndex(index))
         }
         set {
             value.setValue(newValue.value, at: index)
         }
     }
 
-    subscript(dynamicMember member: String) -> JSValue {
+    subscript(dynamicMember member: String) -> Value {
         get {
-            return JSValue(value.forProperty(member)!)
+            return Value(value.forProperty(member)!)
         }
         set {
             value.setValue(newValue.value, forProperty: member)
@@ -172,19 +202,19 @@ public extension JSValue {
     }
 }
 
-public extension JSValue {
+public extension Value {
     @discardableResult
-    func dynamicallyCall(withArguments arguments: [JSValue]) throws -> JSValue {
+    func dynamicallyCall(withArguments arguments: [Value]) throws -> Value {
         let values = arguments.map({ $0.value })
 
         guard let returnValue = value.call(withArguments: values) else {
             throw JavaScriptSwift.Error.notAFunction
         }
+//        add exception catch
+//        if let exception = JavaScriptSwift.context.exception {
+//            throw exception
+//        }
 
-        if let exception = JavaScriptSwift.context.exception {
-            throw exception
-        }
-
-        return JSValue(returnValue)
+        return Value(returnValue)
     }
 }
